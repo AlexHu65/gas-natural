fe81a4905c5dc8569fa514e493ebc74f66b5e5c3
AlexHu65 <alejandrotsu23@gmail.com>
2019-11-07 15:25:45 -0600

    Natg
    Assets

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+  <script src="https://kit.fontawesome.com/8a0ae86e3a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <section class="bloq_1">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-12 col-md-8">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div class="col-sm-12 col-md-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="bloq_2 mt-5 mb-5">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <form id="frm-contact">
+            <div class="form-group">
+              <input type="text" name="nombre" placeholder="Nombre" class="form-control">
+              <span class="help-block"></span>
+            </div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-6">
+                  <input type="text" name="lada" placeholder="Lada" class="form-control">
+                  <span class="help-block"></span>
+                </div>
+                <div class="col-6">
+                  <input type="phone" name="telefono" placeholder="Teléfono" class="form-control">
+                  <span class="help-block"></span>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <input type="text" name="domicilio" placeholder="Domicilio" class="form-control">
+              <span class="help-block"></span>
+            </div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-6">
+                  <input type="text" name="email" placeholder="Correo Electrónico" class="form-control">
+                  <span class="help-block"></span>
+                </div>
+                <div class="col-6">
+                  <input type="text" name="colonia" placeholder="Colonia" class="form-control">
+                  <span class="help-block"></span>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-6">
+                  <select class="form-group" name="Estado">
+                    <option class="form-control">Estado</option>
+                  </select>
+                </div>
+                <div class="col-6">
+                  <select class="form-group" name="Ciudad">
+                    <option class="form-control">Ciudad</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="valid">Aviso de privacidad</label>
+              <input type="checkbox" name="valid" value="true">
+            </div>
+            <div class="form-group">
+              <input type="submit" value="ENVIAR">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="bloq_3 mt-5 mb-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card-info">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card-info">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="bloq_3 mt-5 mb-5">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div class="col-md-8">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="./assets/js/jquery-3.4.1.min" type="text/javascript"></script>
+  <script src="./assets/js/bootstrap.min.js" type="text/javascript"> </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="./assets/js/core.js" type="text/javascript"> </script>
+</body>
+</html>
