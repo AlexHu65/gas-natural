ba6e0583220ec9420e9411ca61555f041c8a91fd
AlexHu65 <alejandrotsu23@gmail.com>
2019-11-08 13:02:53 -0600

    Update to the date
    OK

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="./assets/css/core.css">
   <script src="https://kit.fontawesome.com/8a0ae86e3a.js" crossorigin="anonymous"></script>
+  <title>Naturgy | #BeNaturgy</title>
 </head>
 <body>
   <section class="bloq_1">
@@ -14,8 +15,17 @@
         <div class="col-sm-12 col-md-7">
         </div>
         <div class="col-sm-12 col-md-5">
-          <div class="banner_ng text-center">
-            <img src="./assets/img/ng_logo.png" alt="">
+          <div class="logo_ng text-center d-none d-sm-block">
+            <div class="logo_content">
+              <img src="./assets/img/ng_logo.png" alt="logo ng">
+            </div>
+          </div>
+          <div class="tag_ng d-none d-sm-block">
+            <div class="tag_ng_text">
+              La energía que
+              nos mueve
+              #BeNaturgy
+            </div>
           </div>
         </div>
       </div>
@@ -25,23 +35,22 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <form id="frm-suscripcion">
-            <div class="form-group">
-              <input type="text" name="nombre" placeholder="Nombre" class="form-control">
-              <span class="help-block"></span>
-            </div>
-            <div class="form-group">
-              <div class="row">
-                <div class="col-6">
-                  <div class="input-group">
+          <div class="form-wrapper">
+            <form id="frm-suscripcion">
+              <div class="form-group">
+                <input type="text" name="nombre" placeholder="Nombre" class="form-control">
+                <span class="help-block"></span>
+              </div>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-xs-12 col-sm-12 col-md-6">
                     <input type="text" name="lada" placeholder="Lada" class="form-control">
                     <div class="help">
                       <span class="help-block"></span>
                     </div>
                   </div>
-                </div>
-                <div class="col-6">
-                  <div class="input-group">
+                  <div class="col-xs-12 col-sm-12 col-md-6">
+
                     <input type="phone" name="telefono" placeholder="Teléfono" class="form-control">
                     <div class="help">
                       <span class="help-block"></span>
@@ -49,51 +58,52 @@
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="form-group">
-              <input type="text" name="domicilio" placeholder="Domicilio" class="form-control">
-              <span class="help-block"></span>
-            </div>
-            <div class="form-group">
-              <div class="row">
-                <div class="col-6">
-                  <input type="text" name="email" id="emailInput" placeholder="Correo Electrónico" class="form-control">
-                  <span class="help-block email-span"></span>
-                </div>
-                <div class="col-6">
-                  <input type="text" name="colonia" placeholder="Colonia" class="form-control">
-                  <span class="help-block"></span>
+              <div class="form-group">
+                <input type="text" name="domicilio" placeholder="Domicilio" class="form-control">
+                <span class="help-block"></span>
+              </div>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-xs-12 col-sm-12 col-md-6">
+                    <input type="text" name="email" id="emailInput" placeholder="Correo Electrónico" class="form-control">
+                    <span class="help-block email-span"></span>
+                  </div>
+                  <div class="col-xs-12 col-sm-12 col-md-6">
+                    <input type="text" name="colonia" placeholder="Colonia" class="form-control">
+                    <span class="help-block"></span>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="form-group">
-              <div class="row">
-                <div class="col-6">
-                  <select class="custom-select custom-select-md" name="estado" id="jmr_contacto_estado">
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-sm-12 col-xs-12 col-md-6">
+                    <select class="custom-select-md" name="estado" id="jmr_contacto_estado">
 
-                  </select>
-                </div>
-                <div class="col-6">
-                  <select class="custom-select custom-select-md" name="ciudad"  id="jmr_contacto_municipio">
+                    </select>
+                  </div>
+                  <div class="col-sm-12 col-xs-12 col-md-6">
+                    <select class="custom-select-md" name="ciudad"  id="jmr_contacto_municipio">
 
-                  </select>
+                    </select>
+                  </div>
                 </div>
+                <span class="help-block"></span>
               </div>
-              <span class="help-block"></span>
-            </div>
-            <div class="form-group">
-              <label for="valid">Aviso de privacidad</label>
-              <input type="checkbox" name="valid" value="true">
-              <span class="help-block"></span>
-            </div>
-            <div class="form-group">
-              <input type="submit" class="submit" value="ENVIAR">
-              <span class="loading dn">
-                <i class="fas fa-spinner fa-spin"></i>
-              </span>
+              <div class="form-group">
+                <label for="valid">Aviso de privacidad</label>
+                <input type="checkbox" name="valid" value="true">
+                <span class="help-block"></span> <span>Acepto</span>
+              </div>
+              <div class="form-group">
+                <input type="submit" class="submit" value="ENVIAR">
+                <span class="loading dn">
+                  <i class="fas fa-spinner fa-spin"></i>
+                </span>
 
-            </div>
-          </form>
+              </div>
+            </form>
+          </div>
+
         </div>
       </div>
     </div>
@@ -102,23 +112,47 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <div class="card-info">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <div class="card-info back_1">
+            <h3 class="mb-3 mt-3">Beneficio del gas</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+            </p>
+            <p>
+
+              <a class="btn-anchor" href="#">Contrata <i class="fas fa-chevron-right"></i></a>
+            </p>
           </div>
         </div>
         <div class="col-md-6">
-          <div class="card-info">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <div class="card-info back_2">
+            <h3 class="mb-3 mt-3">Información no regulado(Servicio no regulado y 360)</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              <a class="btn-anchor" href="#">Contrata <i class="fas fa-chevron-right"></i></a>
+
+            </p>
+
           </div>
         </div>
       </div>
     </div>
   </section>
-  <section class="bloq_3 mt-5 mb-5">
+  <section class="bloq_3 mt-7 mb-7">
     <div class="container">
       <div class="row">
-        <div class="col">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="col-xs-12 col-sm-12 col-md-8">
+          <div class="mapa_ng">
+            <img src="./assets/img/ng_mapa.png" alt="mapa">
+
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4">
+          <div class="button_map">
+            <a class="btn-anchor" href="#">Contrata <i class="fas fa-chevron-right"></i></a>
+          </div>
         </div>
       </div>
     </div>
@@ -127,10 +161,34 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <div class="logo_footer mt-5">
+            <img src="./assets/img/ng_logo_foot.png" alt="logo">
+          </div>
         </div>
-        <div class="col-md-8">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <hr>
+        <div class="col-md-8 mt-3 mb-3">
+          <ul class="mt-5 mb-5 footer_menu">
+            <li><a href="#">Formulario</a></li>
+            <li><a href="#">Beneficio</a></li>
+            <li><a href="#">Información</a></li>
+            <li><a href="#">Presencia</a></li>
+            <li class="sub_menu_button"><a class="btn-anchor" href="#">Contrata <i class="fas fa-chevron-right"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <hr class="separator mt-5 mb-5">
+      <div class="row">
+        <div class="col-md">
+          <div class="copy-footer">
+            &copy; Naturgy
+          </div>
+          <div class="social-items float-right">
+            <div class="item"><a href="#"><i class="fab fa-facebook-f"></i></a></div>
+            <div class="item"><a href="#"><i class="fab fa-twitter"></i></a></div>
+            <div class="item"><a href="#"><i class="fab fa-instagram"></i></a></div>
+            <div class="item"><a href="#"><i class="fab fa-linkedin-in"></i></a></div>
+            <div class="item"><a href="#"><i class="fab fa-youtube"></i></a></div>
+          </div>
         </div>
       </div>
     </div>
