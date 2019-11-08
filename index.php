a0311076f11f35297077df47cdfc745dde3d30f1
AlexHu65 <alejandrotsu23@gmail.com>
2019-11-08 16:46:36 -0600

    update
    css

--- index.php
+++ index.php
@@ -9,13 +9,21 @@
   <title>Naturgy | #BeNaturgy</title>
 </head>
 <body>
+  <header>
+    <!-- As a link -->
+    <nav class="navbar navbar-light mobile-navbar">
+      <div class="logo_content p-4 text-center">
+        <img src="./assets/img/ng_logo_foot.png" alt="logo ng">
+      </div>
+    </nav>
+  </header>
   <section class="bloq_1">
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-12 col-md-7">
         </div>
-        <div class="col-sm-12 col-md-5">
-          <div class="logo_ng text-center d-none d-sm-block">
+        <div class="header_banner d-none d-sm-block">
+          <div class="logo_ng text-center">
             <div class="logo_content">
               <img src="./assets/img/ng_logo.png" alt="logo ng">
             </div>
@@ -116,10 +124,8 @@
             <h3 class="mb-3 mt-3">Beneficio del gas</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
             </p>
             <p>
-
               <a class="btn-anchor" href="#">Contrata <i class="fas fa-chevron-right"></i></a>
             </p>
           </div>
